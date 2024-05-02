7582ee512dccd303a66f65c0368b2d40356856e7
VTEXsx <8221vitor@gmail.com>
2024-05-01 21:30:44 -0300

    processos relacionados aos graficos foram comentados

--- main.js
+++ main.js
@@ -168,6 +168,6 @@ for (const formElement of form) {
   }
 }
 
-form.addEventListener("submit", renderProgression);
+// form.addEventListener("submit", renderProgression);
 // calculateButton.addEventListener('click', renderProgression)
 clearFormButton.addEventListener("click", clearForm);
